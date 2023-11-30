b79860a7f61d9035c57d525082c29ed6ac88de82
Athena Economides <athena.economides@uzh.ch>
2023-11-30 11:03:55 +0100

    transform segmentation

--- process/elastix/run_elastix.sh
+++ process/elastix/run_elastix.sh
@@ -1,18 +1,38 @@
 #!/bin/bash
+set -eu
+
+if [ "$#" -ne 3 ]; then
+    echo "run_elastix.sh requires 3 input arguments"
+    exit
+fi
 
 ELASTIX_PATH=/home/neptun/Downloads/elastix
 export LD_LIBRARY_PATH=${ELASTIX_PATH}/lib/
 elastix="${ELASTIX_PATH}/bin/elastix"
+transformix="${ELASTIX_PATH}/bin/transformix"
 
 SCRIPT=$(realpath "$0")
 SCRIPTPATH=$(dirname "$SCRIPT")
 
 out=$1
-input=$2
+input_auto=$2
+input_seg=$3
 
 atlas=${SCRIPTPATH}/"fixed.mhd"
 affine=${SCRIPTPATH}/"affine.txt"
-mkdir -p ${out}
+bspline=${SCRIPTPATH}/"bspline.txt"
 
-${elastix} -out ${out} -f ${atlas} -m ${input} -p ${affine} -threads 64
+outEa=${out}/elastix_affine
+outEb=${out}/elastix_bspline
+outT=${out}/transformix
+mkdir -p ${outEa}
+mkdir -p ${outEb}
+mkdir -p ${outT}
+
+# autofluorescence registration
+${elastix} -out ${outEa} -f ${atlas} -m ${input_auto} -p ${affine} -threads 32
+${elastix} -out ${outEb} -f ${atlas} -m ${input_auto} -p ${bspline} -t0 ${outEa}/TransformParameters.0.txt  -threads 32
+
+# apply transformation to segmentation
+${transformix} -in ${input_seg} -out ${outT} -tp ${outEb}/TransformParameters.0.txt -threads 32
 
