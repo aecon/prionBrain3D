1cf6b778eb05fb41da45870559551e7eef36c846
Athena Economides <athena.economides@uzh.ch>
2023-12-18 12:10:16 +0100

    WIP: registration: generate BE files for autof. and signal channels

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import sys
 from utils.dataset import Dataset, DataCollection
 from utils.preprocessor import preprocess
 from process.segmentation import segment
-from process.registration import register
+from process.registration import register_autofluorescnec_signal
 from process.classification import classify
 #from process.quantification import quantify
 
@@ -18,20 +18,22 @@ for dataset in collection.datasets:
     stop_for_cropping = preprocess(dataset)
     if stop_for_cropping==True:
         print("(main) Stopped for cropping.")
+        print("(main) Use ImageJ to find crop coordinates, store them inside the setup file and rerun.")
         continue
     else:
         print("(main) Continuing with rest of processing ...")
 
     # registration
-    register(dataset)
+    print("(main) Registration ...")
+    register_autofluorescnec_signal(dataset)
 
-    # cell segmentation
-    segment(dataset)
-
-    # classification
-    classify(dataset)
-
-
-    # quantification
-    #quantify(dataset)
+#    # cell segmentation
+#    segment(dataset)
+#
+#    # classification
+#    classify(dataset)
+#
+#
+#    # quantification
+#    #quantify(dataset)
 
--- process/registration.py
+++ process/registration.py
@@ -5,11 +5,13 @@ import img3
 from utils.dataset import Dataset
 
 
-def register(dataset):
+def register_signal_segmented(dataset):
 
     output_directory = dataset.output_directory
+
     autofluorescence_nrrd = dataset.input_nrrd
     segmented_nrrd = dataset.segmented_nrrd
+
     segmented_nrrd_big_endian = dataset.segmented_nrrd + "IJ.nrrd"
     autofluorescence_nrrd_big_endian = dataset.input_nrrd + "IJ.nrrd"
 
@@ -18,14 +20,52 @@ def register(dataset):
     basedir = os.path.abspath(os.getcwd())
 
     # - autofluorescence
-    cmd = "%s --headless -macro process/imagej/save_nrrd.ijm %s,%s" % (imageJpath, (basedir+"/"+autofluorescence_nrrd), (basedir+"/"+autofluorescence_nrrd_big_endian))
+    cmd = "%s --headless -macro process/imagej/save_nrrd.ijm %s,%s" % (imageJpath, (autofluorescence_nrrd), (autofluorescence_nrrd_big_endian))
     os.system(cmd)
 
     # - segmented
-    cmd = "%s --headless -macro process/imagej/save_nrrd.ijm %s,%s" % (imageJpath, (basedir+"/"+segmented_nrrd), (basedir+"/"+segmented_nrrd_big_endian))
+    cmd = "%s --headless -macro process/imagej/save_nrrd.ijm %s,%s" % (imageJpath, (segmented_nrrd), (segmented_nrrd_big_endian))
     os.system(cmd)
 
     # run elstix for brain registration to Allen Brain Anatomical Atlas
     cmd = "./process/elastix/run_elastix.sh %s %s %s" % (output_directory, autofluorescence_nrrd_big_endian, segmented_nrrd_big_endian)
     os.system(cmd)
+
+
+def register_autofluorescnec_signal(dataset):
+
+    output_directory = dataset.output_directory
+
+    autofluorescence_nrrd = dataset.input_autof_nrrd
+    signal_nrrd = dataset.input_nrrd
+
+    autofluorescence_nrrd_big_endian = dataset.input_autof_nrrd + "IJ.nrrd"
+    signal_nrrd_big_endian = dataset.input_nrrd + "IJ.nrrd"
+
+    # generate nrrd input for elastic in big endian order
+    imageJpath = "/home/neptun/Desktop/Fiji_AE.app/ImageJ-linux64"
+    basedir = os.path.abspath(os.getcwd())
+
+    # - autofluorescence
+    if not os.path.isfile(autofluorescence_nrrd_big_endian):
+        cmd = "%s --headless -macro process/imagej/save_nrrd.ijm %s,%s" % (imageJpath, (autofluorescence_nrrd), (autofluorescence_nrrd_big_endian))
+        print("(registration) Converting autofluorescence channel to big endian ...")
+        os.system(cmd)
+    else:
+        print("(registration) Autofluorescence channel (big endian) exists:")
+        print(autofluorescence_nrrd_big_endian)
+
+    # - signal
+    if not os.path.isfile(signal_nrrd_big_endian):
+        cmd = "%s --headless -macro process/imagej/save_nrrd.ijm %s,%s" % (imageJpath, (signal_nrrd), (signal_nrrd_big_endian))
+        print("(registration) Converting signal channel to big endian ...")
+        os.system(cmd)
+    else:
+        print("(registration) Signal channel (big endian) exists:")
+        print(signal_nrrd_big_endian)
+
+
+    # run elstix for brain registration to Allen Brain Anatomical Atlas
+#    cmd = "./process/elastix/run_elastix.sh %s %s %s" % (output_directory, autofluorescence_nrrd_big_endian, signal_nrrd_big_endian)
+#    os.system(cmd)
 
