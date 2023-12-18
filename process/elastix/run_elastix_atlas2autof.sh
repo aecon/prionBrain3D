c29c97bed3e1a4afc13c355888032d242173155f
Athena Economides <athena.economides@uzh.ch>
2023-12-18 12:51:06 +0100

    registration: add atlas to autofluorescence transformation

---
+++ process/elastix/run_elastix_atlas2autof.sh
@@ -0,0 +1,53 @@
+#!/bin/bash
+set -eu
+
+#
+# Registration of atlas onto the autofluorescence channel
+# retult: a transformed atlas file to match the autof. channel
+#
+
+if [ "$#" -ne 2 ]; then
+    echo "run_elastix_atlas2autof.sh requires 2 input arguments:"
+    echo "./run_elastix_atlas2autof.sh <OUTPUT DIRECTORY> <AUTOFLUORESCENCE CHANNEL>"
+    exit
+fi
+
+ELASTIX_PATH=${HOME}/Downloads/elastix
+export LD_LIBRARY_PATH=${ELASTIX_PATH}/lib/
+elastix="${ELASTIX_PATH}/bin/elastix"
+transformix="${ELASTIX_PATH}/bin/transformix"
+
+SCRIPT=$(realpath "$0")
+SCRIPTPATH=$(dirname "$SCRIPT")
+
+out=$1
+input_auto=$2
+echo $out
+echo $input_auto
+
+
+atlas=${SCRIPTPATH}/"ABA_25um_annotation_hemisphere.nrrd"
+atlas_annotation=${SCRIPTPATH}/"ABA_25um_reference_hemisphere.nrrd"
+affine=${SCRIPTPATH}/"affine.txt"
+bspline=${SCRIPTPATH}/"bspline.txt"
+threads=32
+
+outEa=${out}/elastix_affine
+outEb=${out}/elastix_bspline
+outT=${out}/transformix
+mkdir -p ${outEa}
+mkdir -p ${outEb}
+mkdir -p ${outT}
+
+# registration of atlas onto the autofluorescence channel
+${elastix} -out ${outEa} -m ${atlas} -f ${input_auto} -p ${affine} -threads $threads
+${elastix} -out ${outEb} -m ${atlas} -f ${input_auto} -p ${bspline} -t0 ${outEa}/TransformParameters.0.txt  -threads $threads
+
+# transformation of annotation atlas 
+# edit the Bspline file to use for transforming binary segmented data
+cp ${outEb}/TransformParameters.0.txt ${outT}/
+sed -i "/FinalBSplineInterpolationOrder/c\(FinalBSplineInterpolationOrder 0)" ${outT}/TransformParameters.0.txt
+
+# apply transformation to segmentation
+${transformix} -in ${atlas_annotation} -out ${outT} -tp ${outT}/TransformParameters.0.txt -threads $threads
+
--- process/registration.py
+++ process/registration.py
@@ -32,15 +32,12 @@ def register_signal_segmented(dataset):
     os.system(cmd)
 
 
-def register_autofluorescnec_signal(dataset):
+def register_atlas2autofluorescence(dataset):
 
     output_directory = dataset.output_directory
 
     autofluorescence_nrrd = dataset.input_autof_nrrd
-    signal_nrrd = dataset.input_nrrd
-
     autofluorescence_nrrd_big_endian = dataset.input_autof_nrrd + "IJ.nrrd"
-    signal_nrrd_big_endian = dataset.input_nrrd + "IJ.nrrd"
 
     # generate nrrd input for elastic in big endian order
     imageJpath = "/home/neptun/Desktop/Fiji_AE.app/ImageJ-linux64"
@@ -55,17 +52,7 @@ def register_autofluorescnec_signal(dataset):
         print("(registration) Autofluorescence channel (big endian) exists:")
         print(autofluorescence_nrrd_big_endian)
 
-    # - signal
-    if not os.path.isfile(signal_nrrd_big_endian):
-        cmd = "%s --headless -macro process/imagej/save_nrrd.ijm %s,%s" % (imageJpath, (signal_nrrd), (signal_nrrd_big_endian))
-        print("(registration) Converting signal channel to big endian ...")
-        os.system(cmd)
-    else:
-        print("(registration) Signal channel (big endian) exists:")
-        print(signal_nrrd_big_endian)
-
-
     # run elstix for brain registration to Allen Brain Anatomical Atlas
-#    cmd = "./process/elastix/run_elastix.sh %s %s %s" % (output_directory, autofluorescence_nrrd_big_endian, signal_nrrd_big_endian)
-#    os.system(cmd)
+    cmd = "./process/elastix/run_elastix_atlas2autof.sh %s %s" % (output_directory, autofluorescence_nrrd_big_endian)
+    os.system(cmd)
 
